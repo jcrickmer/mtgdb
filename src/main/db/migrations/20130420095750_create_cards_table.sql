1918a054a54502084c15e6ba4447079fad977b7d
Jason Crickmer <jcrickmer@gmail.com>
2013-04-22 05:29:17 -0500

    moved

--- src/main/db/migrations/20130420095750_create_cards_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE cards (
-  id int(11) NOT NULL auto_increment PRIMARY KEY,
-  name VARCHAR(128) NOT NULL,
-  rules_text VARCHAR(1000),
-  created_at DATETIME,
-  updated_at DATETIME
-  ) ENGINE InnoDB  CHARACTER SET utf8;
